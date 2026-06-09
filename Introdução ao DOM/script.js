14a1be4b7a3203f297aa24d2993aa22055a6f23f
Daniel André <daniel.freitas.nascimento@escola.pr.gov.br>
2026-06-09 11:55:31 -0300

    Add files via upload

---
+++ Introdução ao DOM/script.js
@@ -0,0 +1,14 @@
+// O JavaScript é uma Linguagem WEB
+
+// Variáveis - Valores armazenados na memória do compudador
+
+// Funçães -Blocos de códico
+
+function carregar(){
+    //alert("Bem vindo ao JavaScript")
+    var msg = Window.document.getElementById ("msg")
+
+    msg.innerHTML = "Desenvolvendo com DOM"
+}
+   
+
